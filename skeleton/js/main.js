0b8c5bd6ea9866eee75a4498c3b82be7767fc001
Li Hsuan Lu <lihsuanlu@gmail.com>
2017-07-03 15:27:11 -0700

    built grid

---
+++ skeleton/js/main.js
@@ -0,0 +1,9 @@
+const View = require('./ttt-view.js');// require appropriate file
+const Game = require('./game.js');// require appropriate file
+
+$( () => {
+  // Your code here
+  let ttt = $("figure.ttt");
+  let newview = new View;
+  newview.setupBoard();
+});
